ce1a440ad22c8b82e519e30440e9eef98f47cfb7
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-29 16:51:52 -0500

    misc

--- src/main/java/com/ger/utility/LoggingAspect.java
+++ src/main/java/com/ger/utility/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.ger.utility;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,6 +15,16 @@ public class LoggingAspect {
     
     public void logServiceException(Exception exception) throws Exception{
         LOGGER.error(exception.getMessage(), exception);
+    }
+    
+    @Before("execution(* com.ger.api.*.*(..))")
+    public void beforeAdvice() throws Exception {
+        System.out.println("API Endpoint is going to be called.");
+    }
+    
+    @After("execution(* com.ger.api.*.*(..))")
+    public void afterAdvice() throws Exception {
+        System.out.println("API Endpoint was called.");
     }
 
 }
