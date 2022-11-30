c5d5ca513f1277ef9903c5edab6d5ecaa1df4528
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-30 18:06:08 -0500

    misc

--- src/main/java/com/ger/GeryonRestApiSpringApplication.java
+++ src/main/java/com/ger/GeryonRestApiSpringApplication.java
@@ -1,7 +1,5 @@
 package com.ger;
 
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -11,19 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.ger.service.SpotifyService;
 
 @SpringBootApplication
-public class GeryonRestApiSpringApplication implements CommandLineRunner{
-    
+public class GeryonRestApiSpringApplication implements CommandLineRunner {
+
     @Autowired
     @Qualifier("spotifyServiceImpl")
     SpotifyService spotifyService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeryonRestApiSpringApplication.class, args);
-	}
-	
-	@Override 
-	public void run (String... args) {
-	    spotifyService.beginClientCredentialAuthorizationFlow();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeryonRestApiSpringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        spotifyService.beginClientCredentialAuthorizationFlow();
+    }
 
 }
