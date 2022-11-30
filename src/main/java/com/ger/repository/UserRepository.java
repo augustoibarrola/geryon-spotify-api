e7b5a706c971dd2b8e8b20493a1d9f2bddc7123f
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-11-30 17:59:09 -0500

    implemented

--- src/main/java/com/ger/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ger.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ger.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
